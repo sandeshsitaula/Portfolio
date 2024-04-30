1aadd9e69bb5f88b0e5262e6fb099cc11af4a60e
sandeshsitaula <navaltajohan@gmail.com>
2024-04-30 10:22:58 +0545

    updated to handle heritage hive

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,6 +6,7 @@ import carzone from "../../Assets/Projects/carzone.png";
 import ecommerce from "../../Assets/Projects/ecommerce.png";
 import foodonline from "../../Assets/Projects/foodonline.png";
 import adult from "../../Assets/Projects/adult.png";
+import heritage from "../../Assets/Projects/heritage.png";
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -20,12 +21,32 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
+              imgPath={adult}
+              isBlog={false}
+              title="Hundi"
+              description="Hundi is a site for sending and receiving money from one country to another.It also allows to pay various bills like internet,electricity,water etc.And also allows to send topup credits from one user to another which allows to withdraw for actual money."
+              ghLink="https://github.com/bishal189/Hundi"
+              demoLink="http://hundi1.netlify.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
               imgPath={carzone}
               isBlog={false}
               title="CarZone"
               description="A Place to Sell Cars And get Services for Cars like repairing and so on . Developed Using Django "
               ghLink="https://github.com/bishal189/django-car"
               demoLink="http://navalta111.pythonanywhere.com/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={heritage}
+              isBlog={false}
+              title="Heritage Hive"
+              description="Heritage Hive is a online market place for the purpose of promoting the traiditional and antique things and making people accessible to those items."
+              ghLink="https://github.com/bishal189/ecommerce_final"
+              demoLink="http://sandesh1.netlify.app/"
             />
           </Col>
           <Col md={4} className="project-card">
@@ -46,16 +67,6 @@ function Projects() {
               description="A Place to choose the best food around resturants in town.See all the menus and prices offered by various resturants ."
               ghLink="https://github.com/bishal189/foodonline"
               demoLink="http://justpie8.pythonanywhere.com/"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={adult}
-              isBlog={false}
-              title="Hundi"
-              description="Hundi is a site for sending and receiving money from one country to another.It also allows to pay various bills like internet,electricity,water etc.And also allows to send topup credits from one user to another which allows to withdraw for actual money."
-              ghLink="https://github.com/bishal189/Hundi"
-              demoLink="http://hundi1.netlify.app/"
             />
           </Col>
         </Row>
